307728afeb760c78b54e864c33532b42eb1b1087
KellyR <kellyruiz.dev@gmail.com>
2025-02-18 18:56:17 -0800

    tree

--- src/tree.kt
+++ src/tree.kt
@@ -1,3 +1,4 @@
+import kotlin.contracts.contract
 
 class TreeNode(var value : Int) {
     var left: TreeNode? = null
@@ -10,6 +11,7 @@ class TreeLeetcode {
     internal fun printingTre(root : TreeNode?){
 
         if (root == null){
+            print("null->")
             return
         }
         print("${root.value}->")
@@ -26,22 +28,63 @@ class TreeLeetcode {
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
     }
 
+    // 110. Balanced Binary Tree
+    fun isBalanced(root: TreeNode?): Boolean {
+
+        if (root == null){
+            return true
+        }
+
+        fun helper(root: TreeNode?): Int{
+
+            if (root == null){
+                return 0
+            }
+
+            var leftSide = helper(root.left)
+            var rightSide = helper(root.right)
+
+            println("leftSide: ${leftSide}")
+            println("rightSide: ${rightSide}")
+            println("root: ${root.value}")
+
+            if (leftSide == -1 || rightSide == -1){
+                return -1
+            }
+
+            if (Math.abs(leftSide - rightSide) > 1){
+                return -1
+            }
+
+            return 1 + Math.max(leftSide, rightSide)
+
+        }
+
+        if (helper(root) == -1) return false else return true
+
+    }
+
 }
 
 fun main(args : Array<String>){
 
     val testClass = TreeLeetcode()
 
-    var root = TreeNode(3)
-    root.left = TreeNode(9)
-    root.right = TreeNode(20)
-    root.left!!.left = null
-    root.left!!.right = null
-    root.right!!.left = TreeNode(15)
-    root.right!!.right = TreeNode(7)
+    var root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(2)
+    root.left!!.left = TreeNode(3)
+    root.left!!.right = TreeNode(3)
+    root.left!!.left!!.left = TreeNode(4)
+    root.left!!.left!!.right = TreeNode(4)
+
+//    var root = TreeNode(1)
+//    root.left = null
+//    root.right = TreeNode(3)
+//    root.right!!.left = TreeNode(2)
 
     testClass.printingTre(root)
     println()
-    println(testClass.maxDepth(root))
+    println(testClass.isBalanced(root))
 
 }
