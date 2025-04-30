63bc07320ad5f1e357fe067bc02372dc88f9bd55
KellyR <kellyruiz.dev@gmail.com>
2025-04-30 05:56:57 -0700

    uber

--- src/uber.kt
+++ src/uber.kt
@@ -1,2 +1,44 @@
-class uber {
+class Uber {
+
+    // 238. Product of Array Except Self
+    fun productExceptSelf(nums: IntArray): IntArray {
+        // I have 3 cases
+        // * No Zeros in the array (prefix)
+        // * One zero in the array (zero for all value except the zero index)
+        // * More than One Zero (result is zero for the array)
+
+        var multi = 1
+        var zero = -1
+        var zeroCount = 0
+
+        for (i in 0 until nums.size){
+            if (nums[i] == 0){
+                zero = i
+                zeroCount += 1
+                multi = multi * 1
+            } else {
+                multi = multi * nums[i]
+            }
+        }
+
+        val result = IntArray(nums.size)
+
+        if (zeroCount > 1){ // case when we more than two index with 0 value
+            return IntArray(nums.size)
+        } else if (zeroCount == 1){
+            result[zero] = multi
+        } else {
+            for (j in 0 until nums.size){
+                result[j] = multi / nums[j]
+            }
+        }
+
+        return result
+    }
+    
+}
+
+fun main(){
+
+    val testClass = Uber()
 }
