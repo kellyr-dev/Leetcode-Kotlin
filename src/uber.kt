75adb77f78a35e4b2920157dd8b0592b5b6f98ca
KellyR <kellyruiz.dev@gmail.com>
2025-05-01 21:54:36 -0700

    uber

--- src/uber.kt
+++ src/uber.kt
@@ -35,10 +35,46 @@ class Uber {
 
         return result
     }
-    
+
+    // 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+
+    //
 }
 
 fun main(){
 
-    val testClass = Uber()
+    val testClass = MyCalendar()
+    testClass.book(10,20)
+    testClass.book(15,25)
+    testClass.book(20,30)
+}
+
+class MyCalendar() {
+
+    val bookings = ArrayList<Pair<Int,Int>>() // start & end
+
+    // [10, 20], [15, 25], [20, 30],
+    //  [12,18]
+    // put [45, 45] // if (start < iEnd)
+    //  if (start < istar) && (end < istar){
+    // return true
+    //else
+    // return false
+    //
+
+    fun book(startTime: Int, endTime: Int): Boolean {
+
+        for (book in bookings){
+
+            if (startTime < book.second){
+                if ( (endTime > book.first) ){
+                    return false
+                }
+            }
+        }
+        bookings.add( Pair(startTime, endTime) )
+        println("${bookings}")
+        return true
+
+    }
 }
