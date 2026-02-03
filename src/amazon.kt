f8b5180b9d69fa99c540e70f4207ee5e7a88b985
KellyR <kellyruiz.dev@gmail.com>
2026-02-03 17:09:26 -0500

    amazon

--- src/amazon.kt
+++ src/amazon.kt
@@ -21,7 +21,6 @@ class Amazon {
                 // reducing windows
                 while (l < r){
                     if (s[l] != s[r]){
-                        println("Set Removing: ${exists}")
                         exists.remove(s[l])
                         l++
                     }else{
@@ -32,7 +31,6 @@ class Amazon {
             } else {
                 exists.add(s[r])
                 maxLength = max(maxLength, exists.size)
-                println("Set: ${exists}")
             }
 
             r++
@@ -41,7 +39,92 @@ class Amazon {
         return maxLength
     }
 
+    // 2. Add Two Numbers
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
 
+        var list1 = l1
+        var list2 = l2
+        var head = ListNode(-1)
+        val result = head
+        var carry = 0
+
+        while (list1 != null && list2 != null){
+
+            val value = list1.value + list2.value + carry
+
+            if ( value < 10){
+                head.next = ListNode(value)
+                list1 = list1.next
+                list2 = list2.next
+                head = head.next!!
+                carry = 0
+            } else {
+                var resto = (value % 10)
+                head.next = ListNode(resto)
+                head = head.next!!
+                list1 = list1.next
+                list2 = list2.next
+                carry = 1
+            }
+
+        }
+
+        while (list1 != null){
+            var value = (list1!!.value + carry)
+
+            if (value < 10){
+                head.next = ListNode(value)
+                list1 = list1!!.next
+                head = head.next!!
+                carry = 0
+            } else {
+
+                head.next = ListNode(value % 10)
+                list1 = list1.next
+                head = head.next!!
+                carry = 1
+            }
+        }
+
+        while (list2 != null){
+            var value = (list2.value + carry)
+
+            if (value < 10){
+                head.next = ListNode(value)
+                list2 = list2.next
+                head = head.next!!
+                carry = 0
+            } else {
+
+                head.next = ListNode(value % 10)
+                list2 = list2.next
+                head = head.next!!
+                carry = 1
+            }
+        }
+
+        if (carry == 1){
+            head.next = ListNode(1)
+        }
+
+        return result.next
+    }
+
+    // 1. Two Sum
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val table = HashMap<Int, Int>()
+
+        for (i in 0 until nums.size){
+
+            val zero = target - nums[i]
+
+            if (table.containsKey(zero)){
+                return intArrayOf(i, table[zero]!!)
+            } else {
+                table[nums[i]] = i
+            }
+        }
+    }
 }
 
 fun main(args : Array<String>){
